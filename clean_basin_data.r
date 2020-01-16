a249dba3267399c8abf4ec0c32fefbf4c8092162
Lauren Hostert <40471257+lehostert@users.noreply.github.com>
2020-01-15 18:21:37 -0600

    Combine station location and sample data (#2)
    * Combine station info with basin data
    
    * Combine station data with CREP data
    
    * Combine stream attributes with combined basin crep data
    
    * Plot combined data as histogram
    
    * Add .pdf to .gitignore
    
    * Refine histogram plots and add summary stats

--- clean_basin_data.r
+++ clean_basin_data.r
@@ -5,13 +5,20 @@ library(tidyverse)
 # 2- Format data in the same style as CREP fish data
 # 3- Add CREP + Basin data together
 # 4- histogram stream size/order/link per data source
-# 
+#
 # I will need to locate a file that has the stream size for all of the CREP sites
 # I will also need to compile a file that ties the basin surveys to PU_Gap codes in kasky.
 ## See waterbody_stations_2010 csv from ArcGIS Attribute table atrribute table
 # Then join the table of size info with that of the basin + CREP location info via PU_Gap code
+#Godspeed
 
-## Read in basin data from IDNR database and combine into one df
+### Read in basin data from IDNR database and combine into one df
+
+# on Mac Bison location is "smb://INHS-bison.inhs.illinois.edu/"
+# on Windows Bison Location is "//INHS-Bison/"
+# TODO how can I remedy running this in both Windows and Mac?
+# TODO change data file paths to pull from server folder for example:
+# basin_1 <- readxl::read_excel(path = "//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Project_Shelley/Data/basin_survey_data_kaskaskia_il.xlsx", sheet = 1)
 basin_1 <- readxl::read_excel(path = "Data/basin_survey_data_kaskaskia_il.xlsx", sheet = 1)
 basin_2 <- readxl::read_excel(path = "Data/basin_survey_data_kaskaskia_il.xlsx", sheet = 2)
 basin_3 <- readxl::read_excel(path = "Data/basin_survey_data_kaskaskia_il.xlsx", sheet = 3)
@@ -19,28 +26,117 @@ basin_4 <- readxl::read_excel(path = "Data/basin_survey_data_kaskaskia_il.xlsx",
 basin <- bind_rows(basin_1, basin_2, basin_3, basin_4)
 names(basin) <- str_replace_all(names(basin),"[:space:]+", "_")
 names(basin) <- str_to_lower(names(basin))
+basin <- rename(basin, weight = "wt(g)")
+basin <- rename(basin, waterbody_code = waterbody)
 
 
-## Read in waterbody station data from GIS layer
+### Read in waterbody station data from GIS layer
 stations <- read_csv(file = "Data/waterbody_stations_20101104.csv", col_names = T)
 kasky_stations <- stations %>% filter(PU_CODE == "kasky")
 names(kasky_stations) <- str_to_lower(names(kasky_stations))
+#
+kasky_stations$waterbody <- str_to_title(kasky_stations$waterbody)
+kasky_stations$new_loc <- str_to_title(kasky_stations$new_loc)
+kasky_stations$county <- str_to_title(kasky_stations$county)
+#
 kasky_stations<- rename(kasky_stations, station_code = station_co)
+kasky_stations<- rename(kasky_stations, length_segment = length)
+kasky_stations<- rename(kasky_stations, section = section_)
+kasky_stations<- rename(kasky_stations, note = note_)
 
-
-#### Pull Waterbody Code from Waterbody Station:
-basin$dupe <- stringr::str_extract(basin$waterbody, "(?<=[:space:]\\([:space:])[:graph:]+")
+### Pull Waterbody Code from Waterbody Station:
+basin$dupe <- stringr::str_extract(basin$waterbody_code, "(?<=[:space:]\\([:space:])[:graph:]+")
 basin$dupe <- stringr::str_replace(basin$dupe, "\\)", "-")
-basin$station_location <- stringr::str_extract(basin$waterbody_station, "(?<=:).*")  
+basin$station_location <- stringr::str_extract(basin$waterbody_station, "(?<=:).*")
 basin$station_code_wrong <- stringr::str_extract(basin$waterbody_station, ".*(?=:)")
 
-### The following code includes a quoting mechanism \Q...\E that allows you to exactly matach user input b/c all the characters in the ... are exact matches
+### The following code includes a quoting mechanism \Q...\E that allows you to exactly match user input b/c it treats all the characters in the ... are exact matches
 ### See the regular expressions stringr vinettes for more info <https://stringr.tidyverse.org/articles/regular-expressions.html>
 basin$station_code <- stringr::str_extract(basin$station_code_wrong,paste0("(?<=\\Q",basin$dupe,"\\E).*"))
+basin$reach_name <- basin$station_code
+basin <- basin %>% select(-c(station_code_wrong, dupe))
 
-basin <- -c(station_code_wrong, dupe)
+### Combine kasky station info with basin data and simplifly full basin survey dataset to simple dataset
+
+basin <- dplyr::left_join(basin, kasky_stations, by = "station_code")
+basin$gear <- if_else(is.na(basin$gear_used), basin$gear_type, basin$gear_used)
+basin$date <- as.Date(basin$sample_start_date, format = "%m/%d/%Y")
+# basin_data <- basin %>% select(c(pugap_code, reach_name, date, species, count, gear))
+
+#il_fish_traits <- read.csv("//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Data/Illinois_fish_traits_complete.csv", na = "", stringsAsFactors = F)
+
+
+### Read in CREP fish data
+crep <- read_csv("//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Data/Fish_Abundance_Data.csv", na = "", col_names = T)
+names(crep) <- str_to_lower(names(crep))
+crep$date <- as.Date(crep$event_date, format = "%m/%d/%Y")
+crep <- rename(crep, "pugap_code" = "pu_gap_code")
+
+
+### Combine kasky station info with CREP data
+crep_data  <- crep %>% select(c(pugap_code, reach_name, date))
+basin_data <- basin %>% select(c(pugap_code, reach_name, date))
+
+kasky_data_combined <- bind_rows("crep_monitoring" = crep_data, "IDNR_basin_surveys" = basin_data, .id = "data_source")
+kasky_data_combined <- unique(kasky_data_combined[c("data_source","pugap_code", "reach_name", "date")])
+
+### Combine Kasky stream attributes with combined basin data
+stream_attributes_table <- readxl::read_excel("Data/kasky_stream_lines_attributes_table.xlsx", sheet = 1)
+names(stream_attributes_table) <- str_to_lower(names(stream_attributes_table))
+stream_attributes_table <- rename(stream_attributes_table, "pugap_code" = "pu_gapcode")
+stream_attributes_table <- rename(stream_attributes_table, "order" = "order_")
+stream_attributes_table <- rename(stream_attributes_table, "arc" = "arc_")
+
+kasky_data_final <- dplyr::left_join(kasky_data_combined, stream_attributes_table, by = "pugap_code")
+kasky_data_final$data_source <- stringr::str_replace(kasky_data_final$data_source, "crep_monitoring", "CREP Monitoring")
+kasky_data_final$data_source <- stringr::str_replace(kasky_data_final$data_source, "IDNR_basin_surveys", "IDNR Basin Surveys")
+kasky_data_final <- kasky_data_final %>% mutate(data_source = as_factor(data_source))
+
+###Plot it as a histogram
+## Stream Link
+ggplot2::ggplot(kasky_data_final, aes(x = link, color = data_source, fill=data_source)) +
+  geom_histogram(breaks = c(5,10,20,40,80,160), position="identity", alpha=0.5) +
+  theme(legend.position="top") +
+  labs(title="Kaskaskia Basin Community Sampling Locations",x="Stream Link", y = "Count", fill = "Survey Type")
+
+ggplot2::ggplot(kasky_data_final, aes(x = link, fill=data_source)) +
+  geom_histogram(breaks = c(0,1,2,3,4,5,6,7,8,9,10,20,30,40,50,100,200), position="identity", alpha=0.5) +
+  theme(legend.position="top") +
+  labs(title="Kaskaskia Basin Community Sampling Locations",x="Stream Link", y = "Count", fill = "Survey Type")
+
+ggplot2::ggplot(kasky_data_final, aes(x = link, color = data_source, fill=data_source)) +
+  geom_histogram(breaks = c(5,10,20,40,80, 160), position="identity", alpha=0.5)  +
+  facet_grid(data_source ~.) +
+  labs(title="Weight histogram plot",x="Link Size", y = "Count")
+
+## Stream Order
+ggplot2::ggplot(kasky_data_final, aes(x = order, fill= data_source)) +
+      geom_histogram(breaks = c(1,2,3,4,5,6,7,8), position="identity", alpha=0.5) +
+      theme(legend.position="top")+
+      labs(title="Kaskaskia Basin Community Sampling Locations",x="Stream Order", y = "Count", fill = "Survey Type")
+
+ggplot2::ggplot(kasky_data_final, aes(x = order, fill= data_source)) +
+  geom_histogram(binwidth = 1, position="identity", alpha=0.5) +
+  theme(legend.position="top", text = element_text(size=20)) +
+  labs(title="Kaskaskia Basin Community Sampling Locations",x="Stream Order", y = "Count", fill = "Survey Type")
+
+ggplot2::ggsave(Kaskaskia_Basin_Community_Sampling_by_stream_order, device = "tiff")
+###Box Plots
+qplot(data_source, order, data = kasky_data_final, 
+      geom= "boxplot", fill = data_source)
+
+# ##Summary StatS
+kasky_data_final %>% group_by(data_source) %>% 
+  summarise(mean_order = mean(kasky_data_final$order),
+            mean_link = mean(kasky_data_final$link)
+  )
+
+kasky_data_final %>% group_by(data_source) %>% purrr::map(summary)
+# NOTE: Get basin survey data in form with only PU_Gap, Reach, Date, Species, Count
 
 
 
-# TODO: Get Basin stations matched with locality info from kasky_stations
-# TODO: Get basin survey data in form with only PU_Gap, Reach, Date, Species, Count
+
+
+
+
