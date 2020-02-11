7b7de448b6c0f38af46837df2687fca7a0898782
Lauren E. Hostert <laurenehostert@gmail.com>
2020-02-11 12:06:14 -0600

    Mark samples that have a surplus of duplicate records

--- duplication_finder.R
+++ duplication_finder.R
@@ -7,54 +7,36 @@ df <- df %>% drop_na(survey_year)
 
 # If # of duplicates is 1/2 the number in teh group then remove every other record if not do nothing. 
 
-#### Count number of rows per site####
-summarise(group_by(df,survey_year,site, date),length(date))
+#### Count number of observations(ie. rows) per site####
+sample_sum <- summarise(group_by(df, survey_year, site, date), observations = length(date))
 
-
-
-#### Count number of duplicates ####
+#### Count number of duplicate rows per site####
 
 df$dupe <- duplicated(df)
-df %>% group_by(survey_year,site, date) %>% count(dupe) %>% filter(dupe == TRUE)
+duplicate_sum <- df %>% group_by(survey_year,site, date) %>% count(dupe) %>% filter(dupe == TRUE)
 
-
-
+sample_summary <- duplicate_sum %>% select(-c(dupe)) %>% 
+  rename(duplicates = n) %>% 
+  right_join(sample_sum)
+  
 #### IF then ####
 
-if_else( )
+sample_summary$fate <- if_else(sample_summary$duplicates >= 0.5*(sample_summary$observations), "drop", "keep")
 
+#### Now do it for basin data ####
+bdf <- basin
 
+sample_sum_bdf <- summarise(group_by(bdf, survey, survey_year, survey_type, gear, sample_type, reach_name, date), observations = length(date))
+
+bdf$dupe <- duplicated(bdf)
+
+duplicate_sum_bdf <- bdf %>% group_by(survey, survey_year, survey_type, gear, sample_type, reach_name, date) %>% count(dupe) %>% filter(dupe == TRUE)
+
+sample_summary_bdf <- duplicate_sum_bdf %>% 
+  select(-c(dupe)) %>% 
+  rename(duplicates = n) %>% 
+  right_join(sample_sum_bdf)
+
+sample_summary_bdf$fate <- if_else(sample_summary_bdf$duplicates >= 0.5*(sample_summary_bdf$observations), "drop", "keep")
 
 #### BELOW THERE BE DRAGONS ####
-
-nrow(df)
-nrow(unique(df))
-nrow(duplicated(df) == FALSE)
-count(df[duplicated(df),])
-anyDuplicated(df)
-
-df %>% group_by(df,survey_year,site, date) %>% count(duplicated(df))
-
-
-require(dplyr)
-df1 %>% count(Year, Month)
-
-
-
-dupe <- NULL
-data.frame(dupe)
-
-dupe[]<- duplicated(df)
-
-
-
-df[duplicated(df)]
-
-
-
-
-aggregate(list(numdup=rep(1,nrow(df))), df, length)
-
-aggregate()
-
-DT <- data.table(df)
